dbd6bd11b4bc26eba932f3a412d72839c4b7451d
PlaneWhite <92884683+PlaneWhite@users.noreply.github.com>
2021-10-24 13:02:18 +0300

    test

---
+++ wp-content/plugins/typerocket-ui/typerocket/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'cf993cbd645a2918351de3bddb106fe9f0414db5',
+        'name' => 'typerocket/typerocket',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'typerocket/core' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../typerocket/core',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'ede7b6d88cde6006eb55405cdbd8096016f8a5d9',
+            'dev_requirement' => false,
+        ),
+        'typerocket/typerocket' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'cf993cbd645a2918351de3bddb106fe9f0414db5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
